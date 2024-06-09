807cef2eeb9dc57fabea3ffdb0b4c23af2421a89
Karen <karen.borrero@grupodinosol.es>
2024-06-09 12:42:28 +0100

    Hero section 💄 change desingn

--- src/shared/Logo.tsx
+++ src/shared/Logo.tsx
@@ -1,8 +1,7 @@
 import React from "react";
+import Image from "next/image";
 import logoImg from "@/images/logo.png";
 import logoLightImg from "@/images/logo-light.png";
-import LogoSvgLight from "./LogoSvgLight";
-import LogoSvg from "./LogoSvg";
 import Link from "next/link";
 import { StaticImageData } from "next/image";
 
@@ -13,37 +12,38 @@ export interface LogoProps {
 }
 
 const Logo: React.FC<LogoProps> = ({
-  img = logoImg,
-  imgLight = logoLightImg,
-  className = "w-24",
-}) => {
+                                     img = logoImg,
+                                     imgLight = logoLightImg,
+                                     className = "w-24",
+                                   }) => {
   return (
-    <Link
-      href="/"
-      className={`ttnc-logo inline-block text-primary-6000 focus:outline-none focus:ring-0 ${className}`}
-    >
-      <LogoSvgLight />
-      <LogoSvg />
-
-      {/* THIS USE FOR MY CLIENT */}
-      {/* PLEASE UN COMMENT BELLOW CODE AND USE IT */}
-      {/* {img ? (
-        <img
-          className={`block max-h-12 ${imgLight ? "dark:hidden" : ""}`}
-          src={img}
-          alt="Logo"
-        />
-      ) : (
-        "Logo Here"
-      )}
-      {imgLight && (
-        <img
-          className="hidden max-h-12 dark:block"
-          src={imgLight}
-          alt="Logo-Light"
-        />
-      )} */}
-    </Link>
+      <Link
+          href="/"
+          className={`ttnc-logo inline-block text-primary-6000 focus:outline-none focus:ring-0 ${className}`}
+      >
+        {/* THIS USE FOR MY CLIENT */}
+        {/* PLEASE UN COMMENT BELLOW CODE AND USE IT */}
+        {img ? (
+            <Image
+                className={`block max-h-12 ${imgLight ? "dark:hidden" : ""}`}
+                src={img}
+                alt="Logo"
+                layout="intrinsic"
+                quality={100}
+            />
+        ) : (
+            "Logo Here"
+        )}
+        {imgLight && (
+            <Image
+                className="hidden max-h-12 dark:block"
+                src={imgLight}
+                alt="Logo-Light"
+                layout="intrinsic"
+                quality={100}
+            />
+        )}
+      </Link>
   );
 };
 
